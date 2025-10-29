06540bb9e9304a88bc91de4eac8acbb528e23c8c
benoit80000 <66091237+benoit80000@users.noreply.github.com>
2025-10-29 23:29:53 +0100

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ['class'],
-  content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
-  theme: { extend: { colors: { brandA: '#6ae3ff', brandB: '#7d5bff' } } },
+  darkMode: 'class',
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./lib/**/*.{js,ts,jsx,tsx,mdx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brandA: "#7B61FF",
+        brandB: "#00FFFF"
+      },
+      borderRadius: {
+        '2xl': '1rem'
+      }
+    },
+  },
   plugins: [],
 };
